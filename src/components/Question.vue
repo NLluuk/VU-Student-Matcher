e9cf75957b767734a0282885e6af9b111de1c786
Luuk Wagenaar <luuk.wagenaar@atos.net>
2020-10-24 17:58:31 +0200

    initial commit

---
+++ src/components/Question.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-form>
+    <!-- <v-card-title>{{ question.title }}</v-card-title>
+    <v-card-text>{{ question.text }}</v-card-text> -->
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text>{{ text }}</v-card-text>
+    <v-radio-group>
+      <v-radio
+        v-for="n in 4"
+        :key="n"
+        :label="`Option ${n}`"
+        :value="n"
+      ></v-radio>
+    </v-radio-group>
+  </v-form>
+</template>
+
+<script>
+export default {
+  // props: {
+  //   question: Object
+  // },
+  props: ["title", "text"],
+  data: () => ({
+
+  })
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-stepper v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step v-for="n in 15" :key="n" :step="n" :complete="e1 > n">
+        Question {{ n }}
+      </v-stepper-step>
+    </v-stepper-header>
+    <v-stepper-items v-for="n in 15" :key="n">
+      <v-stepper-content :step="n">
+        <question title="Question #1" text="test1"></question>
+      </v-stepper-content>
+    </v-stepper-items>
+    <v-btn color="primary" @click="nextStep">
+      Continue
+    </v-btn>
+    <v-btn text @click="previousStep">
+      Cancel
+    </v-btn>
+  </v-stepper>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Question from "@/components/Question.vue";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  components: { Question },
+  data: () => ({
+    e1: 1,
+    show: true,
+    currentStep: 1,
+    questions: [
+      {
+        title: "This is  test question #1, just fill in the blanks?",
+        text: "This is a not so short test description"
+      },
+      {
+        title: "This is  test question #2, just fill in the blanks?",
+        text: "This is a not so short test description"
+      },
+      {
+        title: "This is  test question #3, just fill in the blanks?",
+        text: "This is a not so short test description"
+      },
+      {
+        title: "This is  test question #4, just fill in the blanks?",
+        text: "This is a not so short test description"
+      },
+      {
+        title: "This is  test question #5, just fill in the blanks?",
+        text: "This is a not so short test description"
+      }
+    ]
+  }),
+  methods: {
+    nextStep() {
+      this.e1 == this.e1++;
+      this.currentStep = this.currentStep++;
+    },
+
+    previousStep() {
+      this.e1 == this.e1--;
+      this.currentStep = this.currentStep--;
+    }
   }
 };
 </script>
+
+<style>
+</style>
