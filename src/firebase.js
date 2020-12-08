e6bb3e5f0b3c6ee1567f6ff906e121d66204f707
Luuk Wagenaar <luuk.wagenaar@atos.net>
2020-12-08 16:34:18 +0100

    revert to normal

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,26 @@ import "firebase/firestore";
 import "firebase/analytics";
 import "firebase/performance";
 
+// const config = {
+//   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.VUE_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+// };
+
 const config = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCw_serwp4nP-gbh5f3gjZttgHiLTbEuCM",
+  authDomain: "vue-student-matcher.firebaseapp.com",
+  databaseURL: "https://vue-student-matcher.firebaseio.com",
+  projectId: "vue-student-matcher",
+  storageBucket: "vue-student-matcher.appspot.com",
+  messagingSenderId: "530114052145",
+  appId: "1:530114052145:web:60d5c9ebcc6df68de2d510",
+  measurementId: "G-5SJ3WRGLBL"
 };
 
 firebase.initializeApp(config);
